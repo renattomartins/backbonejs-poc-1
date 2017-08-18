51088549326f50a1c6d00d2725b66e990046d959
Renato Martins <renatto.martins@gmail.com>
2017-08-18 08:32:36 -0300

    Includes a simple proof with browserify

---
+++ app/main.js
@@ -0,0 +1,9 @@
+var Backbone = require('backbone');
+module.exports = function(){ return Backbone };
+
+// To run:
+// $ node
+// > require("./app/main")
+// [Function]
+// > require("./app/main")()
+// { VERSION: '1.1.2', ... ]
