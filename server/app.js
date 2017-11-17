9433667bdeb37a5393e8b15ea3d3181494ac3c78
Renato Martins <renatto.martins@gmail.com>
2017-11-17 07:59:44 -0200

    Update dependencies

--- server/app.js
+++ server/app.js
@@ -1,11 +1,8 @@
-// First, we require Express.js, logger, path and stich as dependencies
 var express = require('express'),
     logger = require('morgan'),
-    // a helper to resolve relative paths
     path = require('path'),
     stitch = require('stitch');
 
-// To "stitch" the client-side modules together we create a package
 var package = stitch.createPackage({
     paths: [__dirname + '/../app'],
     dependencies: [
@@ -15,25 +12,14 @@ var package = stitch.createPackage({
     ]
 });
 
-// Then we initialize the application...
 var app = express();
 app.use(express.static(__dirname + '/public'));
-
-// Whenever a request goes to the client, we deliver the modules as client.js
 app.get('/static/bundle.js', package.createServer());
-
-// Make log
-app.use(logger("dev", {
-    immediate: true
-}));
-
-// We add a basic route that servers an index.html
-// ... let's use the same as above
+app.use(logger("dev", { immediate: true }));
 app.get('/', function(req, res) {
     var html = path.resolve(__dirname + '/../index.html');
     res.sendFile(html);
 });
 
-// Let's listen on port 5000
 app.listen(5000);
 console.log("Server is running.");
